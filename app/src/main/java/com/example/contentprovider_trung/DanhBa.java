4d3ce349104aeba941ea32aea50e4e6a92cad7f8
NgoNhatAnhTrung <ngonhatanhtrung@gmail.com>
2022-10-24 10:47:53 +0700

    first commit

---
+++ app/src/main/java/com/example/contentprovider_trung/DanhBa.java
@@ -0,0 +1,89 @@
+package com.example.contentprovider_trung;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.contentprovider_trung.model.Contact;
+import com.example.contentprovider_trung.model.Contact;
+
+import java.util.ArrayList;
+
+public class DanhBa extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_ASK_PERMISSIONS= 1001;
+
+    ListView lvDanhBa;
+    ArrayList<Contact> dsDanhBa;
+    ArrayAdapter<Contact> adapterDanhBa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_ba2);
+
+        if (ContextCompat.checkSelfPermission(
+                DanhBa.this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(DanhBa.this,new String[]{""+
+                    "android.permission.READ_CONTACTS"},1001);
+        }
+        AnhXa();
+
+        lvDanhBa.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intentD=new Intent(DanhBa.this, com.example.contentprovider_trung.Detail.class);
+                Contact contact= dsDanhBa.get(i);
+                intentD.putExtra("ten",contact.getName());
+                intentD.putExtra("so",contact.getPhone());
+                startActivity(intentD);
+            }
+        });
+
+        ShowAllContact();
+
+    }
+
+    private void ShowAllContact() {
+        Uri uri= ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+        Cursor cursor= getContentResolver().query(uri, null,null,null,null);
+        dsDanhBa.clear();
+        while (cursor.moveToNext()){
+
+            String tenCotName=ContactsContract.Contacts.DISPLAY_NAME;
+            String tenCotPhone=ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+            int vitriCotName=cursor.getColumnIndex(tenCotName);
+            int vitriCotPhone=cursor.getColumnIndex(tenCotPhone);
+
+            String name=cursor.getString(vitriCotName);
+            String phone=cursor.getString(vitriCotPhone);
+
+            Contact contact =new Contact(name,phone);
+            dsDanhBa.add(contact);
+            adapterDanhBa.notifyDataSetChanged();
+        }
+    }
+
+    private void AnhXa() {
+        lvDanhBa=findViewById(R.id.lvDanhBa);
+        dsDanhBa=new ArrayList<>();
+        adapterDanhBa= new ArrayAdapter<>(
+                DanhBa.this, android.R.layout.simple_list_item_1,dsDanhBa
+        );
+        lvDanhBa.setAdapter(adapterDanhBa);
+    }
+}
